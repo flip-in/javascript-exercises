e16605ea564fe67b2f342b2682600d2475226e8b
Will Price <pricew86@gmail.com>
2019-07-03 10:11:00 +0900

    finished helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str='hey', num=3) {
+    
+    if(num < 0){
+        return 'ERROR';
+    }
+    
+    else{
+        let newStr= '';
+        for (let i = 0; i<num; i++){
+            newStr+= str;
+        }
+        return newStr;
+    }
+    
+    
 }
-
 module.exports = repeatString
