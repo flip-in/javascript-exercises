7556f9f52e414e2c496461fea425d33fa587f3a3
Will Price <pricew86@gmail.com>
2019-07-04 09:57:34 +0900

    finished reverseString. All tests passed.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str='string') {
+    let newStr= "";
+    for(i in str){
+        newStr += str.slice(str.length-1);
+        str = str.slice(0,str.length-1);
+    }
+    console.log(newStr);
+    return newStr;
 }
 
 module.exports = reverseString
