fa83712d637e0f164f8d1e2b7803f48f43a6c503
Will Price <pricew86@gmail.com>
2019-07-08 11:35:30 +0900

    finished removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
-
+    newArray = arguments[0];
+    console.log(newArray);
+    //(outer)for loop goes through arguments starting after the array
+    outer:for (i=1; i<arguments.length; i++){
+        //(inner)for loop goes through the array looking for value
+        //at position argument[i]
+        inner:for(j in newArray){
+            //console.log(newArray[j]);
+            if (newArray[j] == arguments[i]){
+                //console.log(newArray[j]);
+                newArray.splice(j, 1);
+                break;
+            }
+        }
+        console.log(newArray);    
+    }
+    return newArray;
 }
 
 module.exports = removeFromArray
