c1a6801002af92e27da5908524e89e8570f958ab
Will Price <pricew86@gmail.com>
2019-07-09 10:39:50 +0900

    solved sumAll with while loop and math equation.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,42 @@
 const sumAll = function() {
-
+    let sum = 0
+    for (i in arguments){
+        if (arguments[i] < 0 ){
+            return "ERROR";
+        }
+        if(typeof arguments[i] !== "number"){
+            return "ERROR";
+        }
+        else{
+            continue;
+        }
+    }
+    /*******Solved with While Loop******************/
+    if (arguments[0] < arguments[1]){
+        var start = arguments[0];
+        var end = arguments[1];
+    }
+    else{
+        var start = arguments[1];
+        var end = arguments[0];
+    }
+    while(start <= end){
+        sum += start;
+        start++;
+    }
+    /********Solved with Math equation********* */
+    /*
+    if (arguments[0] < arguments[1]){
+        sum += arguments[1]*(arguments[1]+arguments[0]) / 2;
+    }
+    else{
+        sum += arguments[0]*(arguments[0]+arguments[1]) / 2;
+    }
+    */
+    console.log(sum);
+    return sum;
 }
 
 module.exports = sumAll
+
+
